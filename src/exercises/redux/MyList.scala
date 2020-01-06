2c794d5dd52f00df5e33fffac97cb8f462c607c1
Iain A Maxwell <imaxwell@scottlogic.com>
2020-01-06 12:15:31 +0000

    Making redone list generic

--- src/exercises/redux/MyList.scala
+++ src/exercises/redux/MyList.scala
@@ -1,10 +1,10 @@
 package exercises.redux
 
-abstract class MeList {
-  def head: Int
-  def tail: MeList
+abstract class MeList[+A] {
+  def head: A
+  def tail: MeList[A]
   def isEmpty: Boolean
-  def add(i: Int): MeList
+  def add[B >: A](i: B): MeList[B]
   def printElements: String
 
   override def toString: String = {
@@ -12,19 +12,19 @@ abstract class MeList {
   }
 }
 
-object Empty extends MeList {
-  override def head: Int = throw new NoSuchElementException
-  override def tail: MeList = throw new NoSuchElementException
+object Empty extends MeList[Nothing] {
+  override def head: Nothing = throw new NoSuchElementException
+  override def tail: MeList[Nothing] = throw new NoSuchElementException
   override def isEmpty: Boolean = true
-  override def add(i: Int): MeList = new Cons(i, Empty)
+  override def add[B >: Nothing](i: B): MeList[B] = new Cons(i, Empty)
   override def printElements = ""
 }
 
-class Cons(h: Int, t: MeList) extends MeList {
-  override def head: Int = h
-  override def tail: MeList = t
+class Cons[+A](h: A, t: MeList[A]) extends MeList[A] {
+  override def head: A = h
+  override def tail: MeList[A] = t
   override def isEmpty: Boolean = false
-  override def add(i: Int): MeList = new Cons(i, this)
+  override def add[B >: A](i: B): MeList[B] = new Cons[B](i, this)
   override def printElements =
     if (t.isEmpty) "" + h
     else h + " " + t.printElements
@@ -34,9 +34,9 @@ object Demo extends App {
   val empty = Empty
   println(empty)
 
-  val one: MeList = Empty.add(1)
-  val two = one.add(2)
-  val three = two.add(3)
+  val one: MeList[Double] = Empty.add(6.419)
+  val two = one.add(3.14258)
+  val three = two.add(1)
 
   println(three)
 }
