0c161076d2e2289614cb4185aaf0763dab92765f
maxp-dlg <ian.maxwell@directlinegroup.co.uk>
2020-02-04 12:02:43 +0000

    Implementing slice to use tail recursive function rather than existing utility methods

--- src/problems99/ElevenToTwenty.scala
+++ src/problems99/ElevenToTwenty.scala
@@ -1,13 +1,25 @@
 package problems99
 
 object ElevenToTwenty {
-  def slice[A](start: Int, end: Int, list: List[A]): List[A] =
+  def slice[A](start: Int, end: Int, list: List[A]): List[A] = {
+
+    def _slice[A](cutFromLeft: Int, cutFromRight: Int, _remaining: List[A]): List[A] = (cutFromLeft, cutFromRight, _remaining) match {
+      case (0, 0, _remaining) => _remaining
+      case (0, cutFromRight, _remaining) => _slice(cutFromLeft, cutFromRight - 1, _remaining.init)
+      case (cutFromLeft, cutFromRight, _remaining) => {
+        _slice(cutFromLeft - 1, cutFromRight, _remaining.tail)
+      }
+
+    }
+
     if (list == null) throw new IllegalStateException("Cannot split null list")
     else if (list.isEmpty) throw new IllegalStateException("Cannot split empty list")
     else if (start < 0 || start >= list.length) throw new IndexOutOfBoundsException("Start Index is outwith list ranges")
     else if (end < 0 || end > list.length) throw new IndexOutOfBoundsException("End Index is outwith list ranges")
     else if (end < start) throw new IndexOutOfBoundsException("End Index must not be smaller than start index")
-    else list.take(end).takeRight(end - start)
+    //    else list.take(end).takeRight(end - start)
+    _slice(start, list.length - end, list)
+  }
 
   def splitInTwain[A](x: Int, n: List[A]): (List[A], List[A]) =
     if (n == null) throw new IllegalStateException("Cannot split null list")
