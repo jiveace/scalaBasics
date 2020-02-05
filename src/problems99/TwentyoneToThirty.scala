353ca8bbc21eefe6305d547bef969870062705d7
maxp-dlg <ian.maxwell@directlinegroup.co.uk>
2020-02-05 12:07:43 +0000

    Comments and refactoring

--- src/problems99/TwentyoneToThirty.scala
+++ src/problems99/TwentyoneToThirty.scala
@@ -3,14 +3,16 @@ package problems99
 import scala.util.Random
 
 object TwentyoneToThirty {
+  // 25
   def randomise[A](input: List[A]): List[A] = {
     randomSelect(input, input.length)
   }
 
-
+  // 24
   def lotto(n: Int, rangeValues: (Int, Int)): List[Int] =
     randomSelect(range(rangeValues._1, rangeValues._2), n)
 
+  // 23
   def randomSelect[A](list: List[A], x: Int): List[A] = {
     def _randomSelect[A](acc: List[A], pool: List[A]): List[A] =
       if (acc.size == x) acc
@@ -22,10 +24,11 @@ object TwentyoneToThirty {
     _randomSelect(List(), list)
   }
 
-
+  // 22
   def insertAt[A](list: List[A], slot: Int, value: A): List[A] =
     (list.take(slot - 1) :+ value) ::: list.takeRight(list.length + 1 - slot)
 
+  // 21
   def range(x: Int, y: Int): List[Int] = {
     def _range(_list: List[Int], next: Int): List[Int] = {
       if (next > y) _list
