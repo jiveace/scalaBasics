739fe501d4f6873126006a42ae7482b6582c79f6
maxp-dlg <ian.maxwell@directlinegroup.co.uk>
2020-02-13 11:45:48 +0000

    Refactoring validation function

--- src/problems99/Roman.scala
+++ src/problems99/Roman.scala
@@ -3,6 +3,7 @@ package problems99
 object Roman {
 
   def toInt(romans: String) = {
+
     def mapDigit(str: String): Int = str match {
       case "I" => 1
       case "V" => 5
@@ -10,23 +11,27 @@ object Roman {
       case _ => throw new IllegalStateException("Unsupported denomination")
     }
 
-    def _validate(str: String): String =
-      if (str.split("").count(_ == "V") > 1)
-        throw new IllegalStateException("Too many Vs")
-      else if (str.contains("IIIIIIIIII"))
-        throw new IllegalStateException("Too many consecutive Is")
-      else if (str.contains("XXXXXXXXXX"))
-        throw new IllegalStateException("Too many consecutive Is")
-      else str
+    def _validate(str: String): String = {
+      def checkForTenConsecutive(n: String) =
+        if (str.contains(n * 10))
+          throw new IllegalStateException(s"Input includes 10 consecutive ${n}s")
 
-    def _toInt(list: List[Int], acc: Int): Int =
-      if (list.size == 1) acc + list.last
-      else if (list(0) < list(1)) _toInt(list.tail, acc - list(0))
-      else _toInt(list.tail, acc + list(0))
+      def checkForMultiplesOf(n: String): Unit =
+        if (str.split("").count(_ == n) > 1)
+          throw new IllegalStateException(s"Multiple ${n}s are not allowed")
+
+      checkForMultiplesOf("V")
+      checkForTenConsecutive("I")
+      checkForTenConsecutive("X")
+      str
+    }
+
+    def _toInt(list: Array[Int], total: Int): Int =
+      if (list.size == 1) total + list.last
+      else if (list(0) < list(1)) _toInt(list.tail, total - list(0))
+      else _toInt(list.tail, total + list(0))
 
     if (romans == null || romans.isEmpty) 0
-    else _toInt(_validate(romans).split("").map(mapDigit(_)).toList, 0)
+    else _toInt(_validate(romans).split("").map(mapDigit(_)), 0)
   }
-
-
 }
