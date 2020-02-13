64398bbdfad2502fecbcb753cdd7dca19ca4b021
maxp-dlg <ian.maxwell@directlinegroup.co.uk>
2020-02-13 11:31:47 +0000

    Support for V and X

--- src/problems99/Roman.scala
+++ src/problems99/Roman.scala
@@ -1,17 +1,32 @@
 package problems99
 
 object Roman {
+
   def toInt(romans: String) = {
+    def mapDigit(str: String): Int = str match {
+      case "I" => 1
+      case "V" => 5
+      case "X" => 10
+      case _ => throw new IllegalStateException("Unsupported denomination")
+    }
+
     def _validate(str: String): String =
       if (str.split("").count(_ == "V") > 1)
         throw new IllegalStateException("Too many Vs")
+      else if (str.contains("IIIIIIIIII"))
+        throw new IllegalStateException("Too many consecutive Is")
+      else if (str.contains("XXXXXXXXXX"))
+        throw new IllegalStateException("Too many consecutive Is")
       else str
 
-    if (romans == null) 0
-    else _validate(romans).split("").count(_ == "I") +
-      romans.split("").count(_ == "V") * 5
-  }
+    def _toInt(list: List[Int], acc: Int): Int =
+      if (list.size == 1) acc + list.last
+      else if (list(0) < list(1)) _toInt(list.tail, acc - list(0))
+      else _toInt(list.tail, acc + list(0))
 
+    if (romans == null || romans.isEmpty) 0
+    else _toInt(_validate(romans).split("").map(mapDigit(_)).toList, 0)
+  }
 
 
 }
--- test/problems99/RomanTest.scala
+++ test/problems99/RomanTest.scala
@@ -1,39 +1,68 @@
 package problems99
 
 import org.scalatest.flatspec.AnyFlatSpec
+import problems99.Roman.toInt
 
 class RomanTest extends AnyFlatSpec {
 
   "translate" must "return 0 for null input" in {
-    assert(Roman.toInt(null) == 0)
+    assert(toInt(null) == 0)
   }
 
   "translate" must "return 0 for empty input" in {
-    assert(Roman.toInt("") == 0)
+    assert(toInt("") == 0)
+  }
+
+  "translate" must "throw exception for unsupported denomination" in {
+    assertThrows[IllegalStateException](toInt("G") == 0)
+    assertThrows[IllegalStateException](toInt("U") == 0)
+    assertThrows[IllegalStateException](toInt("A") == 0)
+    assertThrows[IllegalStateException](toInt("M") == 0)
   }
 
   "translate" must "count Is properly" in {
-    assert(Roman.toInt("I") == 1)
-    assert(Roman.toInt("III") == 3)
-    assert(Roman.toInt("IIIII") == 5)
+    assert(toInt("I") == 1)
+    assert(toInt("III") == 3)
+    assert(toInt("IIIII") == 5)
+  }
+
+  "translate" must "not allow more then nine  Is in a row" in {
+    assertThrows[IllegalStateException](toInt("IIIIIIIIII"))
+    assertThrows[IllegalStateException](toInt("IIIIIIIIIIIII"))
   }
 
   "translate" must "count V as five" in {
-    assert(Roman.toInt("V") == 5)
+    assert(toInt("V") == 5)
   }
 
   "translate" must "count V followed by Is" in {
-    assert(Roman.toInt("VI") == 6)
-    assert(Roman.toInt("VIIII") == 9)
-    assert(Roman.toInt("VIIIIIIII") == 13)
+    assert(toInt("VI") == 6)
+    assert(toInt("VIIII") == 9)
+    assert(toInt("VIIIIIIII") == 13)
   }
 
-//  "translate" must "count V preceded by I" in {
-//    assert(Roman.toInt("IV") == 4)
-//  }
+  "translate" must "count V preceded by I" in {
+    assert(toInt("IV") == 4)
+  }
 
   "translate" must "throw an exception for multiple Vs" in {
-    assertThrows[IllegalStateException](Roman.toInt("VV"))
+    assertThrows[IllegalStateException](toInt("VV"))
+  }
+
+  "translate" must "count X as ten" in {
+    assert(toInt("X") == 10)
+    assert(toInt("XX") == 20)
+    assert(toInt("XXX") == 30)
+  }
+
+  "translate" must "count X preceded by a I" in {
+    assert(toInt("IX") == 9)
+  }
+
+  "translate" must "not allow more than 9 consecutive Xs" in {
+    assert(toInt("XXXXXXXXX") == 90)
+    assertThrows[IllegalStateException](toInt("XXXXXXXXXX"))
+    assertThrows[IllegalStateException](toInt("XXXXXXXXXxxX"))
   }
 
   "translate" must "throw an exception for multiple Ds" in {
@@ -48,8 +77,8 @@ class RomanTest extends AnyFlatSpec {
 
   "translate" must "throw an exception for unordered numerals" in {
     "IIV" == "BOOM"
+    "VX" == "BOOM"
   }
-
 
 
 }
