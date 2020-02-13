e73a7bd009c4f18376549079f27ac62603bf3462
maxp-dlg <ian.maxwell@directlinegroup.co.uk>
2020-02-13 11:49:25 +0000

    Renaming test cases

--- test/problems99/RomanTest.scala
+++ test/problems99/RomanTest.scala
@@ -5,77 +5,77 @@ import problems99.Roman.toInt
 
 class RomanTest extends AnyFlatSpec {
 
-  "translate" must "return 0 for null input" in {
+  "toInt" must "return 0 for null input" in {
     assert(toInt(null) == 0)
   }
 
-  "translate" must "return 0 for empty input" in {
+  "toInt" must "return 0 for empty input" in {
     assert(toInt("") == 0)
   }
 
-  "translate" must "throw exception for unsupported denomination" in {
+  "toInt" must "throw exception for unsupported denomination" in {
     assertThrows[IllegalStateException](toInt("G") == 0)
     assertThrows[IllegalStateException](toInt("U") == 0)
     assertThrows[IllegalStateException](toInt("A") == 0)
     assertThrows[IllegalStateException](toInt("M") == 0)
   }
 
-  "translate" must "count Is properly" in {
+  "toInt" must "count Is properly" in {
     assert(toInt("I") == 1)
     assert(toInt("III") == 3)
     assert(toInt("IIIII") == 5)
   }
 
-  "translate" must "not allow more then nine  Is in a row" in {
+  "toInt" must "not allow more then nine Is in a row" in {
     assertThrows[IllegalStateException](toInt("IIIIIIIIII"))
     assertThrows[IllegalStateException](toInt("IIIIIIIIIIIII"))
   }
 
-  "translate" must "count V as five" in {
+  "toInt" must "count V as five" in {
     assert(toInt("V") == 5)
   }
 
-  "translate" must "count V followed by Is" in {
+  "toInt" must "count V followed by Is" in {
     assert(toInt("VI") == 6)
     assert(toInt("VIIII") == 9)
     assert(toInt("VIIIIIIII") == 13)
   }
 
-  "translate" must "count V preceded by I" in {
+  "toInt" must "count V preceded by I" in {
     assert(toInt("IV") == 4)
   }
 
-  "translate" must "throw an exception for multiple Vs" in {
+  "toInt" must "throw an exception for multiple Vs" in {
     assertThrows[IllegalStateException](toInt("VV"))
   }
 
-  "translate" must "count X as ten" in {
+  "toInt" must "count X as ten" in {
     assert(toInt("X") == 10)
     assert(toInt("XX") == 20)
     assert(toInt("XXX") == 30)
   }
 
-  "translate" must "count X preceded by a I" in {
+  "toInt" must "count X preceded by a I" in {
     assert(toInt("IX") == 9)
   }
 
-  "translate" must "not allow more than 9 consecutive Xs" in {
+  "toInt" must "not allow more than 9 consecutive Xs" in {
     assert(toInt("XXXXXXXXX") == 90)
     assertThrows[IllegalStateException](toInt("XXXXXXXXXX"))
     assertThrows[IllegalStateException](toInt("XXXXXXXXXxxX"))
   }
 
-  "translate" must "throw an exception for multiple Ds" in {
+  "toInt" must "throw an exception for multiple Ds" in {
     "DD" == 5
     "DDD" == 5
   }
 
-  "translate" must "throw an exception for multiple Ls" in {
+  "toInt" must "throw an exception for multiple Ls" in {
     "LL" == "BOOM"
     "LLL" == "BOOM"
   }
 
-  "translate" must "throw an exception for unordered numerals" in {
+  "toInt" must "throw an exception for unordered numerals" in {
     "IIV" == "BOOM"
     "VX" == "BOOM"
   }
